11e78512bd38d57d77524dfa74cbb7c4cf5c136c
Anish Prashun <66175058+Anishpras@users.noreply.github.com>
2022-03-26 14:37:03 +0530

    components done

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,17 @@
+import Card from "../components/Card";
+import Header from "../components/Header";
+
+const style = {
+  wrapper: `h-screen w-screen flex flex-col bg-[#222229]`,
+  cardsContainer: `flex flex-col items-center justify-center flex-1`,
+};
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className={style.wrapper}>
+      <Header />
+      <div className={style.cardsContainer}>
+        <Card />
+      </div>
+    </div>
+  );
 }
